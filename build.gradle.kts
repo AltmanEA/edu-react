06a426134b2741cbc8f2f1aeb62f08a2369b01d5
Evgenii Altman <altmanea@gmail.com>
2021-08-11 11:41:43 +0600

    update library version

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,12 +12,12 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-react:17.0.2-pre.206-kotlin-1.5.10")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:17.0.2-pre.206-kotlin-1.5.10")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-styled:5.3.0-pre.206-kotlin-1.5.10")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-router-dom:5.2.0-pre.206-kotlin-1.5.10")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-redux:4.0.5-pre.206-kotlin-1.5.10")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-redux:7.2.3-pre.206-kotlin-1.5.10")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-react:17.0.2-pre.229-kotlin-1.5.21")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:17.0.2-pre.229-kotlin-1.5.21")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-styled:5.3.0-pre.229-kotlin-1.5.21")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-router-dom:5.2.0-pre.229-kotlin-1.5.21")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-redux:4.1.0-pre.229-kotlin-1.5.21")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-redux:7.2.4-pre.229-kotlin-1.5.21")
 }
 
 kotlin {
