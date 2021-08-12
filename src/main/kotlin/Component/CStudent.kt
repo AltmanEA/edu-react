df7c2c611caf082cc48e819158192fd274bf0d44
Evgenii Altman <altmanea@gmail.com>
2021-08-12 12:10:20 +0600

    lecture component part two

--- src/main/kotlin/Component/CStudent.kt
+++ src/main/kotlin/Component/CStudent.kt
@@ -1,21 +1,52 @@
 package Component
 
 import data.Student
+import kotlinx.html.js.onClickFunction
 import react.*
 import react.dom.*
 
-external interface StudentProps : RProps {
+external interface StudentProps : Props {
     var student: Student
 }
 
-val CStudent = fc { props: StudentProps ->
-    div("student") {
-        props.student.let {
-            +"${it.firstname} ${it.surname}"
-        }
-    }
+external interface StudentState : State {
+    var marked: Boolean
 }
 
-fun RBuilder.student(student:Student) = child(CStudent) {
-    attrs.student = student
+class CStudent : RComponent<StudentProps, StudentState>(){
+    init {
+        state.apply {
+            marked = false
+        }
+    }
+    override fun RBuilder.render() {
+        div(
+            if (state.marked) "marked" else "unmarked"
+        ) {
+            +"${props.student.firstname} ${props.student.surname}"
+            attrs.onClickFunction = {
+                setState{
+                    marked = !marked
+                }
+            }
+        }
+    }
+
 }
+
+fun RBuilder.student(student: Student) =
+    child(CStudent::class) {
+        attrs.student = student
+    }
+
+//val CStudent = fc { props: StudentProps ->
+//    div("student") {
+//        props.student.let {
+//            +"${it.firstname} ${it.surname}"
+//        }
+//    }
+//}
+//
+//fun RBuilder.student(student:Student) = child(CStudent) {
+//    attrs.student = student
+//}
