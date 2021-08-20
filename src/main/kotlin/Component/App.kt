b9bfc94c48f1b8080add5d24a2adc56de6ac86a3
Evgenii Altman <altmanea@gmail.com>
2021-08-20 13:15:25 +0600

    lecture many component part two template component

--- src/main/kotlin/Component/App.kt
+++ src/main/kotlin/Component/App.kt
@@ -1,6 +1,7 @@
 package Component
 
 import react.Props
+import react.RBuilder
 import react.dom.h1
 import react.fc
 import react.useState
@@ -8,11 +9,24 @@ import shortOutput
 
 val CApp = fc<Props>("App"){
     val (mode, setMode) = useState("Full")
+    val (marked, setMarked) = useState(data.studentList.map { false })
+    val elements = data.studentList.zip(marked)
+    fun onClick(index: Int){
+        setMarked{
+            it.mapIndexed {_index, mark ->
+                if (index==_index)
+                    !mark
+                else
+                    mark
+            }
+        }
+    }
     modePicker(mode, setMode)
     shortOutput.Provider(mode){
         h1 {
             +"List of student"
         }
-        studentList(data.studentList.toTypedArray())
+        markedList(elements, ::onClick, RBuilder::student)
+//        studentList(data.studentList.toTypedArray())
     }
 }
--- src/main/kotlin/Component/CStudent.kt
+++ src/main/kotlin/Component/CStudent.kt
@@ -1,31 +1,26 @@
 package Component
 
 import data.Student
-import kotlinx.html.js.onClickFunction
-import org.w3c.dom.events.Event
 import react.*
-import react.dom.div
+import react.dom.span
 import shortOutput
 
 external interface StudentProps : Props {
     var student: Student
     var marked: Boolean
-    var onClick: (Event) -> Unit
 }
 
 val CStudent = fc ("Student") { props: StudentProps ->
-    div(if (props.marked) "marked" else "unmarked") {
+    span(if (props.marked) "marked" else "unmarked") {
         if(useContext(shortOutput)=="Full")
             +props.student.fullName
         else
             +props.student.shortName
-        attrs.onClickFunction = props.onClick
     }
 }
 
-fun RBuilder.student(student: Student, marked: Boolean, onClick: (Event) -> Unit) =
+fun RBuilder.student(student: Student, marked: Boolean) =
     child(CStudent) {
         attrs.student = student
         attrs.marked = marked
-        attrs.onClick = onClick
     }
